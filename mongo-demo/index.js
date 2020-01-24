3e1f7924cd21a2f7a4f14acf88a213e3b400527d
Said <saidadem3@gmail.com>
2020-01-23 22:31:13 -0600

    folder refactor and mongo start

---
+++ mongo-demo/index.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+mongoose
+  .connect('mongodb://localhost/playground')
+  .then(() => console.log('Connected to MongoDB...'))
+  .catch(err => console.error('Could not connect to MongoDB', err));
+
+const courseSchema = new mongoose.Schema({
+  name: String,
+  author: String,
+  tags: [String],
+  date: { type: Date, default: Date.now },
+  isPublished: Boolean
+});
+
+const Course = mongoose.model('Course', courseSchema);
+const course = new Course({
+  name: 'Node.js Course',
+  author: 'Mosh',
+  tags: ['node', 'backend'],
+  isPublished: true
+});
